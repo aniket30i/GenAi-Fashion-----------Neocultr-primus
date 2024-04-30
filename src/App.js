8f6848cc42489885529227898112515238e3e1bd
aniket30i <iamaniket.saha.09@protonmail.com>
2024-04-30 16:44:42 +0530

    the advertising section is also done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
+import Vibey from "./Components/Vibey";
 
 function App() {
   return (
     <div className="main">
       <Navbar />
       <Hero />
-      <div className="description-p1"></div>
+      <Vibey />
       <div className="description-p2"></div>
       <div className="mid-hero"></div>
       <div className="running-img"></div>
---
+++ src/Components/Vibey.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Vibey() {
+  return (
+    <div className="description-p1">
+      <div className="dp-1">
+        <h2>
+          LETS EMBARK ON A VISIONARY JOURNEY WHERE TRADITION AND TECHNOLOGY
+          CONVERGE , CRAFTING A FUTURE WHERE FASHION TRANSCENDS BOUNDARIES.
+        </h2>
+      </div>
+      <div className="dp-11">
+        <p>
+          Inspired by the rich narratives of global cultures and powered by the
+          latest AI innovation, let create our own vision of fashion exploring
+          time, tradition, tomorrow.
+        </p>
+      </div>
+      <div className="dp-2">
+        <p className="running-txt">
+          A CELEBRATION OF HERITAGE MESHED WITH THE HORIZON OF POSSIBILITIES
+          INVITING YOU TO DISCOVER A TAPESTRY OF IMAGINATION THAT REDEFINE THE
+          ESSENCE OF STYLE.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Vibey;
