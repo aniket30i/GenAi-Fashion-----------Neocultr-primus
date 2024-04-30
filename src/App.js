03ee2d5b9112690ea61de94e20b2c63c7bd7dc5c
aniket30i <iamaniket.saha.09@protonmail.com>
2024-04-30 13:13:12 +0530

    Making a GenAi something

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Navbar />
+      <Hero />
+      <div className="description-p1"></div>
+      <div className="description-p2"></div>
+      <div className="mid-hero"></div>
+      <div className="running-img"></div>
+      <div className="end-card"></div>
     </div>
   );
 }
