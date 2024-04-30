0790e04cdfbb5009a20b2d0dc9cd99c4358e7449
aniket30i <iamaniket.saha.09@protonmail.com>
2024-05-01 03:14:31 +0530

    made some fixes

--- src/App.js
+++ src/App.js
@@ -26,10 +26,40 @@ function App() {
         </h1>
       </Hero>
       <Running />
-
-      <div className="end-card"></div>
+      <div className="footer">
+        <h1>UNITE</h1>
+        <div className="footer-txts">
+          <footer>
+            <div class="footer-content">
+              <h4>Instagram</h4>
+              <h4>X (twitter)</h4>
+              <h4>
+                Local time:
+                <h4 id="current-time">12:30:45</h4>
+              </h4>
+              <h4>&copy; 2024 Aniket Saha</h4>
+            </div>
+          </footer>
+        </div>
+      </div>
     </div>
   );
 }
+
+function TimeNow() {
+  var now = new Date();
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  var currentTime = hours + ":" + minutes + ":" + seconds;
+  document.getElementById("current-time").textContent = currentTime;
+}
+setInterval(TimeNow, 1000);
+TimeNow();
 
 export default App;
--- src/Components/Running.js
+++ src/Components/Running.js
@@ -1,7 +1,7 @@
 import React from "react";
 import t1 from "../ideas/typ1.jpg";
-import t2 from "../ideas/typ1.jpg";
-import t3 from "../ideas/typ1.jpg";
+import t2 from "../ideas/typ2.jpg";
+import t3 from "../ideas/typ3.jpg";
 import t4 from "../ideas/typ4.jpg";
 import t5 from "../ideas/typ5.jpg";
 import t6 from "../ideas/typ6.jpg";
@@ -14,43 +14,15 @@ function Running() {
         <div class="con">
           <img className="ron-pic" src={t1} alt="xyz" />
           <div id="gola"></div>
-          <img className="ron-pic" src={t4} alt="xyz" />
-          <div id="gola"></div>
-          <img className="ron-pic" src={t5} alt="xyz" />
-          <div id="gola"></div>
-          <img className="ron-pic" src={t6} alt="xyz" />
-          <div id="gola"></div>
           <img className="ron-pic" src={t2} alt="xyz" />
           <div id="gola"></div>
           <img className="ron-pic" src={t3} alt="xyz" />
-          <div id="gola"></div>
-        </div>
-        <div class="con">
-          <img className="ron-pic" src={t1} alt="xyz" />
           <div id="gola"></div>
           <img className="ron-pic" src={t4} alt="xyz" />
           <div id="gola"></div>
           <img className="ron-pic" src={t5} alt="xyz" />
           <div id="gola"></div>
           <img className="ron-pic" src={t6} alt="xyz" />
-          <div id="gola"></div>
-          <img className="ron-pic" src={t2} alt="xyz" />
-          <div id="gola"></div>
-          <img className="ron-pic" src={t3} alt="xyz" />
-          <div id="gola"></div>
-        </div>
-        <div class="con">
-          <img className="ron-pic" src={t1} alt="xyz" />
-          <div id="gola"></div>
-          <img className="ron-pic" src={t4} alt="xyz" />
-          <div id="gola"></div>
-          <img className="ron-pic" src={t5} alt="xyz" />
-          <div id="gola"></div>
-          <img className="ron-pic" src={t6} alt="xyz" />
-          <div id="gola"></div>
-          <img className="ron-pic" src={t2} alt="xyz" />
-          <div id="gola"></div>
-          <img className="ron-pic" src={t3} alt="xyz" />
           <div id="gola"></div>
         </div>
       </div>
